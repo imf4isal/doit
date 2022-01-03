4ff6383ad40e598c562506668d5abfe483c0b761
Faisal <md.faisal1899@yahoo.com>
2022-01-03 11:56:08 +0600

    added delete item functionality

--- src/js/app.js
+++ src/js/app.js
@@ -4,7 +4,6 @@ const itemSubmit = document.querySelector(".todo-item-submit");
 const itemLists = document.querySelector(".todo-item-lists");
 
 const item = document.querySelector(".todo-item");
-const itemDelete = document.querySelector(".todo-item-delete");
 
 itemSubmit.addEventListener("click", function (e) {
   e.preventDefault();
@@ -12,12 +11,37 @@ itemSubmit.addEventListener("click", function (e) {
   const itemValue = itemInput.value;
 
   const itemm = `
-        <li class="todo-item">
-            <span class="todo-item-title">${itemValue}</span>
-            <span class="todo-item-delete todo-item-btn">delete</span>
-        </li>`;
+    <li class="todo-item">
+    <span class="todo-item-title">${itemValue}</span>
+    <span class="todo-item-delete todo-item-btn">delete</span>
+    </li>`;
 
   itemLists.insertAdjacentHTML("afterbegin", itemm);
-  console.log(itemValue);
-  console.log(itemm);
+
+  const itemDelete = document.querySelector(".todo-item-delete");
+
+  itemDelete.addEventListener("click", deleteItem);
+
+  setDefault();
+  //   activeDelete();
+
+  //   itemDelete.addEventListener("click", function () {
+  //     console.log("delete");
+  //   });
 });
+
+// const activeDelete = function () {
+//   itemDelete.addEventListener("click", function (e) {
+//     console.log("delete item");
+//     // e.target.closest(".todo-item").remove();
+//   });
+// };
+
+const deleteItem = function (e) {
+  const element = e.target.closest(".todo-item");
+  element.remove();
+};
+
+const setDefault = function () {
+  itemInput.value = "";
+};
